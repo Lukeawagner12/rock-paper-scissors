a129bf149427d0500dac48f64b54741204dcab5c
Luke Wagner <lukeawagner12@gmail.com>
2024-01-29 22:10:46 -0500

    Add game logic to main.js file

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function getComputerChoice() {
-    var randomNumber = Math.floor(Math.random() * 4) + 1;
+    var randomNumber = Math.floor(Math.random() * 3) + 1;
     var computerChoice = "";
     console.log(randomNumber);
     if (randomNumber === 1) {
-        ComputerChoice = "Rock"
+        computerChoice = "Rock"
     } else if (randomNumber === 2) {
         computerChoice = "Paper"
     } else if (randomNumber === 3) {
@@ -12,4 +12,44 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+function getUserChoice() {
+    var userChoice = prompt("Choose your element!");
+    return userChoice;
+}
+
+var userChoice = getUserChoice();
+var computerChoice = getComputerChoice();
+
+function determineWinner(userChoice, computerChoice) {
+    if (userChoice === "Rock" && computerChoice === "Scissors") {
+        youWin();
+    } else if (userChoice === "Rock" && computerChoice === "Paper") {
+        youLoose();
+    }
+
+    if (userChoice === "Paper" && computerChoice === "Rock") {
+        youWin();
+    } else if (userChoice === "Paper" && computerChoice === "Scissors") {
+        youLoose();
+    }
+
+    if (userChoice === "Scissors" && computerChoice === "Paper") {
+        youWin();
+    } else if (userChoice === "Scissors" && computerChoice === "Rock") {
+        youLoose();
+    } 
+
+    if (userChoice === computerChoice) {
+        draw();
+    }
+
+    function youLoose() {
+        console.log("You loose! " + computerChoice + " beats " + userChoice);
+    }
+    function youWin() {
+        console.log("You win! " + userChoice + " beats " + computerChoice);
+    }
+    function draw() {
+        console.log("It's a draw! " + userChoice + " and " + computerChoice + " are the same...try again!");
+    }
+}
