318944900cce3e9ef653ec6d33e6d8b2c38dcb34
Luke Wagner <lukeawagner12@gmail.com>
2024-03-25 14:43:37 -0400

    Made UI for RPS game

--- main.js
+++ main.js
@@ -1,82 +1,114 @@
-var numberOfRounds = prompt("Choose how many rounds you want to play.");
-console.log(typeof numberOfRounds);
-numberOfRounds = Number(numberOfRounds);
-console.log(typeof numberOfRounds);
-count = 0;
-var computerScore = 0;
-var userScore = 0;
-while (numberOfRounds > count) {
-    function getComputerChoice() {
-        var randomNumber = Math.floor(Math.random() * 3) + 1;
-        var computerChoice = "";
-        console.log(randomNumber);
-        if (randomNumber === 1) {
-            computerChoice = "Rock"
-        } else if (randomNumber === 2) {
-            computerChoice = "Paper"
-        } else if (randomNumber === 3) {
-            computerChoice = "Scissors"
-        }
-        return computerChoice;
-    }
-    
-    function getUserChoice() {
-        var userChoice = prompt("Choose your element!");
-        return userChoice[0].toUpperCase() + userChoice.slice(1).toLowerCase();
-    }
-    
-    var userChoice = getUserChoice();
-    console.log(userChoice);
-    var computerChoice = getComputerChoice();
-    console.log(computerChoice);
-    determineWinner(userChoice, computerChoice);
-    
-    function determineWinner(userChoice, computerChoice) {
-        if (userChoice === "Rock" && computerChoice === "Scissors") {
-            youWin();
-        } else if (userChoice === "Rock" && computerChoice === "Paper") {
-            youLoose();
-        }
-    
-        if (userChoice === "Paper" && computerChoice === "Rock") {
-            youWin();
-        } else if (userChoice === "Paper" && computerChoice === "Scissors") {
-            youLoose();
-        }
-    
-        if (userChoice === "Scissors" && computerChoice === "Paper") {
-            youWin();
-        } else if (userChoice === "Scissors" && computerChoice === "Rock") {
-            youLoose();
-        } 
-    
-        if (userChoice === computerChoice) {
-            draw();
-        }
-    
-        function youLoose() {
-            console.log("You loose! " + computerChoice + " beats " + userChoice);
-            computerScore++;
-        }
-        function youWin() {
-            console.log("You win! " + userChoice + " beats " + computerChoice);
-            userScore++;
-        }
-        function draw() {
-            console.log("It's a draw! " + userChoice + " and " + computerChoice + " are the same...try again!");
-        }
-    }
-    count++;
-}
-determineUltimateWinner(computerScore, userScore)
+let userScoreText = document.querySelector('#user-score');
+let computerScoreText = document.querySelector('#computer-score');
+let drawScoreText = document.querySelector('#draw-score');
+let startButton = document.querySelector('#start');
+let restartButton = document.querySelector('#restart');
+let buttons = document.querySelectorAll('.button');
+let gameWrapper = document.querySelector('.gameWrapper');
+let buttonContainer = document.querySelector('.button-container');
+let finalScore = document.querySelector('.final-score');
+let computerChoiceText = document.querySelector('.computer-choice');
+// Set scores to zero
+let draw = 0;
+let userScore = 0;
+let computerScore = 0;
+let numOfGames = prompt("How many rounds do you want to play?");
+numOfGames = Number(numOfGames);
+let count = 0;
 
-function determineUltimateWinner(computerScore, userScore) {
-    console.log("Final score is computer: " + computerScore + " | You: " + userScore + "!");
-    if (computerScore > userScore) {
-        console.log("You are the ultimate loser!");
-    } else if (computerScore < userScore) {
-        console.log("You are the ultimate winner!");
+// Start button to start game
+startButton.addEventListener('click', function() {
+   gameWrapper.classList.remove('hidden');
+   restartButton.classList.remove('hidden');
+   this.classList.add('hidden');
+});
+
+// Restart Button
+restartButton.addEventListener('click', function() {
+    location.reload();
+});
+
+// Add click event listener to all buttons
+buttons.forEach(button => {
+    button.addEventListener('click', function(event) {
+        let userChoice = event.target.value;
+        runGame(userChoice);
+    });
+});
+
+function runGame(userChoice) {
+    if(count < numOfGames) {
+        // Get computer choice
+        let computerChoice = getComputerChoice();
+        // Detmerine winner
+        let winner = determineWinner(userChoice, computerChoice);
+        // Update the score
+        updateScore(winner, userScore, computerScore, draw);
+        // Update front end score board to reflect new score
+        userScoreText.textContent = userScore;
+        computerScoreText.textContent = computerScore;
+        drawScoreText.textContent = draw;
+        console.log(winner);
+
+        function updateScore() {
+            if(winner === "User") {
+                userScore++;
+            } else if (winner === "Computer") {
+                computerScore++;
+            } else {
+                draw++;
+            }
+        }
+        count++;
+        if (count == numOfGames) {
+            buttonContainer.classList.add('hidden');
+            if(userScore > computerScore) {
+                finalScore.textContent = "You are the winner!";
+            } else if(computerScore > userScore) {
+                finalScore.textContent = "You lost!";
+            } else {
+                finalScore.textContent = "Its a draw!";
+            }
+        }
     } else {
-        console.log("It was a draw!");
+        console.log("Game Over");
+    }
+}
+
+function getComputerChoice() {
+    var randomNumber = Math.floor(Math.random() * 3) + 1;
+    var computerChoice = "";
+    if (randomNumber === 1) {
+        computerChoice = "Rock"
+    } else if (randomNumber === 2) {
+        computerChoice = "Paper"
+    } else if (randomNumber === 3) {
+        computerChoice = "Scissors"
+    }
+    computerChoiceText.textContent = "Computer chose " + computerChoice + "!";
+    return computerChoice;
+}
+
+function determineWinner(userChoice, computerChoice) {
+    if (userChoice === "Rock" && computerChoice === "Scissors") {
+        return "User";
+    } else if (userChoice === "Rock" && computerChoice === "Paper") {
+        return "Computer";
+    }
+
+    if (userChoice === "Paper" && computerChoice === "Rock") {
+        return "User";
+    } else if (userChoice === "Paper" && computerChoice === "Scissors") {
+        return "Computer";
+    }
+
+    if (userChoice === "Scissors" && computerChoice === "Paper") {
+        return "User";
+    } else if (userChoice === "Scissors" && computerChoice === "Rock") {
+        return "Computer";
+    } 
+
+    if (userChoice === computerChoice) {
+        return "draw";
     }
 }
